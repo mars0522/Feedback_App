660886fb05e68fb60f6b6db6f9eb7b254a94affb
varun Pratap <varun.singh01@indiamart.com>
2022-04-14 15:24:46 +0530

    updated

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,31 +1,40 @@
 import React, { useState } from 'react'
 import Button from './Shared/Button';
 import Card from './Shared/Card'
+import Rating from './Rating';
 
 function FeedbackForm() {
 
     const [text, setText] = useState('');
     const [btnDisabled, setbtnDisabled] = useState(true);
     const [message, setmessage] = useState('');
+    const [rating, setRating] = useState(10);
 
     const changeHandler = (event) => {
         if (text === '') {
             setbtnDisabled(true);
-            setmessage(null);
+            setmessage('Feedback should be atleast 10 charactes long');
         } else if (text !== '' && text.trim().length >= 10) {
             setbtnDisabled(false);
-            setmessage('Feedback should be atleast 10 charactes long');
+            setmessage();
         } else {
             setbtnDisabled(true);
-            setmessage(null);
+            setmessage('Feedback should be atleast 10 charactes long');
         }
         setText(event.target.value);
+    }
+
+    const ratingfun = (input_rating) => {
+        setRating(input_rating);
+        console.log(rating);
     }
 
   return (
       <Card>
           <form>
               <h2>How would you rate our servercie?</h2>
+              <Rating rating={ratingfun}/>
+
               <div className='input-group'>
                   <input  onChange={changeHandler} type='text' placeholder='Write your review' />
                   <Button type='submit' isDisabled={btnDisabled}>Send</Button>
